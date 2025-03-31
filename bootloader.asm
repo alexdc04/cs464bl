a7371b812636ae0c2e3938ac93098a8346c93bb3
Alexander Chang <154938082+alexdc04@users.noreply.github.com>
2025-03-31 12:11:07 -0400

    add comments + tag for 16 bit real mode
    comments added to bootloader file

--- bootloader.asm
+++ bootloader.asm
@@ -1,11 +1,12 @@
-[org 0x7c00]
-    mov ah, 0x0E
-    mov al, 'H'
-    int 0x10
-    mov al, 'i'
-    int 0x10
-    cli
-    hlt
+[bits 16]           ; begin code in 16-bit real mode
+[org 0x7c00]        ; load code at address 0x7c00
+    mov ah, 0x0E    ; trigger BIOS teletype mode to print chars to screen
+    mov al, 'H'     ; load character H into address AL (last byte of accumulator)
+    int 0x10        ; call BIOS interrupt 0x10, prints char in AL
+    mov al, 'i'     
+    int 0x10        ; repeat above steps for character i
+    cli             ; clear interrupts
+    hlt             ; stop processor
 
-times 510 - ($ - $$) db 0
-dw 0xAA55
+times 510 - ($ - $$) db 0 ; pad the rest of the file with zeros to reach required file size
+dw 0xAA55                 ; boot signature, tells BIOS that disk is bootable
